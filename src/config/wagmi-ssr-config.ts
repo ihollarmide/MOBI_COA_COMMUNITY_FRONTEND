ecc8f6f5cbdd83291559fc4ed96a5ae7dfef5a91
ihollarmide <olamideganiyuidris@gmail.com>
2025-08-13 10:03:25 +0100

    feat: add fallback rpc

--- src/config/wagmi-ssr-config.ts
+++ src/config/wagmi-ssr-config.ts
@@ -4,7 +4,13 @@ import { baseSepolia, bsc } from "wagmi/chains";
 export const wagmiSSRConfig = createConfig({
   chains: [bsc, baseSepolia],
   transports: {
-    [bsc.id]: http("https://bsc-dataseed3.defibit.io"),
+    // [bsc.id]: http(
+    //   "https://bnb-mainnet.g.alchemy.com/v2/TCAngl9Gxs-7GF9JFRcJJaiY-Le3vdhK"
+    // ),
+    // [baseSepolia.id]: http(
+    //   "https://base-sepolia.g.alchemy.com/v2/VZp21oJ4tRhkRpONwkRGs"
+    // ),
+    [bsc.id]: http("https://bsc-rpc.publicnode.com"),
     [baseSepolia.id]: http("https://sepolia.base.org"),
   },
   ssr: true,
--- src/providers/ethers-provider.tsx
+++ src/providers/ethers-provider.tsx
@@ -1,7 +1,10 @@
 import { ethers } from "ethers";
 
 export const provider = new ethers.JsonRpcProvider(
+  // process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+  //   ? "https://bnb-mainnet.g.alchemy.com/v2/TCAngl9Gxs-7GF9JFRcJJaiY-Le3vdhK"
+  //   : "https://base-sepolia.g.alchemy.com/v2/VZp21oJ4tRhkRpONwkRGs"
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-    ? "https://bnb-mainnet.g.alchemy.com/v2/TCAngl9Gxs-7GF9JFRcJJaiY-Le3vdhK"
-    : "https://base-sepolia.g.alchemy.com/v2/VZp21oJ4tRhkRpONwkRGs"
+    ? "https://bsc-rpc.publicnode.com"
+    : "https://sepolia.base.org"
 );
